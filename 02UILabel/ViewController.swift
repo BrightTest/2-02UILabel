a4f7249a5cb7f75c2f22f929bdf769ba40f76da8
BrightTest <zl0930c10@163.com>
2019-03-15 22:33:05 +0800

    创建一个标签控件

--- 02UILabel/ViewController.swift
+++ 02UILabel/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let label = UILabel(frame: CGRect(x: 20, y: 100, width: 280, height: 30))
+        label.text = "Hello World!"
+        self.view.addSubview(label)
     }
 
 
